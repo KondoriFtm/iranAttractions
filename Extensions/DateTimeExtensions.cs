a077c3c8609c8f88b2432d6e122cefd0c03605a7
Fatemeh Kondori <kondoriftm@gmail.com>
2024-12-29 16:31:24 +0330

    add footer and some cjanges

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace iranAttractions.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToShamsi(this DateTime dateTime, bool includeTime = false)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            int year = persianCalendar.GetYear(dateTime);
+            int month = persianCalendar.GetMonth(dateTime);
+            int day = persianCalendar.GetDayOfMonth(dateTime);
+
+            string shamsiDate = $"{year}/{month:D2}/{day:D2}";
+
+            if (includeTime)
+            {
+                string time = dateTime.ToString("HH:mm:ss"); // Format the time as HH:mm:ss
+                shamsiDate = $"{shamsiDate} {time}";
+            }
+
+            return shamsiDate;
+        }
+    }
+}
